4507ed42c78d3bf487cb6fc6a54f13f0b2c73447
Kayllane  Farias <75839810+Aninimo@users.noreply.github.com>
2023-04-06 12:47:54 -0300

    Create constants.js

---
+++ client/src/config/constants.js
@@ -0,0 +1,37 @@
+
+export const EditorTabs = [
+  {
+    name: "colorpicker",
+    icon: 'https://raw.githubusercontent.com/adrianhajdin/project_threejs_ai/main/client/src/assets/swatch.png'
+  },
+  {
+    name: "filepicker",
+    icon: 'https://raw.githubusercontent.com/adrianhajdin/project_threejs_ai/main/client/src/assets/file.png',
+  },
+  {
+    name: "aipicker",
+    icon: 'https://raw.githubusercontent.com/adrianhajdin/project_threejs_ai/main/client/src/assets/ai.png',
+  },
+];
+
+export const FilterTabs = [
+  {
+    name: "logoShirt",
+    icon: 'https://raw.githubusercontent.com/adrianhajdin/project_threejs_ai/main/client/src/assets/logo-tshirt.png',
+  },
+  {
+    name: "stylishShirt",
+    icon: 'https://raw.githubusercontent.com/adrianhajdin/project_threejs_ai/main/client/src/assets/stylish-tshirt.png',
+  },
+];
+
+export const DecalTypes = {
+  logo: {
+    stateProperty: "logoDecal",
+    filterTab: "https://raw.githubusercontent.com/adrianhajdin/project_threejs_ai/main/client/src/assets/logo-tshirt.png",
+  },
+  full: {
+    stateProperty: "fullDecal",
+    filterTab: "https://raw.githubusercontent.com/adrianhajdin/project_threejs_ai/main/client/src/assets/stylish-tshirt.png",
+  },
+};
